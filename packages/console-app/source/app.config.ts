1a5ff7cd0f40e2eb470ee61bf4e6e56adf0231f3
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-12 01:23:12 +0100

    Don't provide noop ng zone

--- packages/console-app/source/app.config.ts
+++ packages/console-app/source/app.config.ts
@@ -2,8 +2,7 @@ import { ApplicationConfig } from '@angular/core'
 import { provideRouter } from '@angular/router'
 
 import { routes } from './app.routes'
-import { provideNoopNgZone } from './app.providers'
 
 export const config: ApplicationConfig = {
-  providers: [provideRouter(routes), provideNoopNgZone()],
+  providers: [provideRouter(routes)],
 }
