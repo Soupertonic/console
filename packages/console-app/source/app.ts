324f5e7036712e71653e6edbb34d03c07ae18235
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-12 02:03:31 +0100

    Run format

--- packages/console-app/source/app.ts
+++ packages/console-app/source/app.ts
@@ -3,6 +3,4 @@ import { bootstrapApplication } from '@angular/platform-browser'
 import { config } from './app.config'
 import { AppComponent } from './app.component'
 
-bootstrapApplication(AppComponent, config).catch((err: any) =>
-  console.error(err)
-)
+bootstrapApplication(AppComponent, config).catch((err: any) => console.error(err))
