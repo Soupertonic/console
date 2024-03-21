ceea508671ff2a7ee2f693eb12f836c72f8945d5
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-21 23:47:20 +0100

    Let's get going (Take 2)

--- packages/console-app/source/app.component.ts
+++ packages/console-app/source/app.component.ts
@@ -1,7 +1,5 @@
-import { Component, ViewEncapsulation, signal } from '@angular/core'
+import { Component, ViewEncapsulation } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
-import { Meter, MeterLabel, MeterValue } from './components/meter'
-import { ProgressBar } from './components/progress-bar'
 
 @Component({
   standalone: true,
@@ -9,16 +7,6 @@ import { ProgressBar } from './components/progress-bar'
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   encapsulation: ViewEncapsulation.ShadowDom,
-  imports: [RouterOutlet, Meter, MeterLabel, MeterValue, ProgressBar],
+  imports: [RouterOutlet],
 })
-export class AppComponent {
-  currentPlayerCount = signal(0)
-  maximumPlayerCount = signal(100)
-
-  f() {
-    this.currentPlayerCount.update((v) => v + 2)
-  }
-  b() {
-    this.currentPlayerCount.update((v) => v - 2)
-  }
-}
+export class AppComponent {}
