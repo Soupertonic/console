f6ca88b664f58f508bd6cb02339f60b51805dfbb
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-22 22:33:10 +0100

    Remove obsolete features

--- packages/console-ui/source/components/meter/meter.component.ts
+++ packages/console-ui/source/components/meter/meter.component.ts
@@ -1,6 +1,4 @@
 import { Component, input } from '@angular/core'
-import { MeterLabel } from './meter-label.component'
-import { MeterValue } from './meter-value.component'
 import { ProgressBar } from '../progress-bar'
 
 @Component({
@@ -14,15 +12,4 @@ export class Meter {
   public minimum = input.required<number>()
   public mean = input.required<number>()
   public maximum = input.required<number>()
-
-  protected label: MeterLabel | undefined
-  protected value: MeterValue | undefined
-
-  public assignLabel(label: MeterLabel): void {
-    this.label = label
-  }
-
-  public assignValue(value: MeterValue): void {
-    this.value = value
-  }
 }
