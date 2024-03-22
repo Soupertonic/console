58df6bd6d31a7740525ad438ee389c3b4e68c167
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-22 10:25:03 +0100

    Introduce uptime and vitals game server boards

--- packages/console-app/source/pages/gameserver/gameserver.component.ts
+++ packages/console-app/source/pages/gameserver/gameserver.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core'
 import { RouterLink, RouterOutlet } from '@angular/router'
 
+import { GameserverUptimeBoard } from './components/uptime-board'
+import { GameserverVitalsBoard } from './components/vitals-board'
+
 @Component({
   standalone: true,
   selector: 'console-gameserver',
   templateUrl: 'gameserver.component.html',
   styleUrl: 'gameserver.component.css',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, GameserverUptimeBoard, GameserverVitalsBoard],
 })
 export class Gameserver {}
