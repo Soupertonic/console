062c6ce9e5ef7e0fb17ecbd696fd6ee52efbf2ba
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-23 00:29:05 +0100

    Add data components to board

--- packages/console-app/source/pages/gameserver/components/uptime-board/gameserver-uptime-board.component.ts
+++ packages/console-app/source/pages/gameserver/components/uptime-board/gameserver-uptime-board.component.ts
@@ -1,10 +1,12 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core'
+import { Data, DataLabel, DataValue } from '@console/ui/components/data';
 
 @Component({
   standalone: true,
   selector: 'console-gameserver-uptime-board',
   templateUrl: 'gameserver-uptime-board.component.html',
   styleUrl: 'gameserver-uptime-board.component.scss',
-  imports: [],
+  imports: [Data, DataLabel, DataValue, CurrencyPipe],
 })
 export class GameserverUptimeBoard {}
