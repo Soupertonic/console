dd388506980a4c764284c6670014aef5916c7700
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-22 17:05:32 +0100

    Display vitals using console meters

--- packages/console-app/source/pages/gameserver/components/vitals-board/gameserver-vitals-board.component.ts
+++ packages/console-app/source/pages/gameserver/components/vitals-board/gameserver-vitals-board.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
+import { Meter, MeterLabel, MeterValue } from '../../../../../../console-ui/source/components/meter'
 
 @Component({
   standalone: true,
   selector: 'console-gameserver-vitals-board',
   templateUrl: 'gameserver-vitals-board.component.html',
   styleUrl: 'gameserver-vitals-board.component.css',
-  imports: [],
+  imports: [Meter, MeterLabel, MeterValue],
 })
 export class GameserverVitalsBoard {}
