c77bdfabbe37501a6035157bd1954bd4d4128cf5
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-22 20:28:33 +0100

    Remove unused imports

--- packages/console-app/source/pages/gameserver/gameserver.component.ts
+++ packages/console-app/source/pages/gameserver/gameserver.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core'
-import { RouterLink, RouterOutlet } from '@angular/router'
 
 import { GameserverActionsBoard } from './components/actions-board'
 import { GameserverIdBoard } from './components/id-board'
