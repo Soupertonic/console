f914444038b3a491fc91d083304b4112d00591ef
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-22 01:35:08 +0100

    Add basic routing

--- packages/console-app/source/pages/gameserver/gameserver.component.ts
+++ packages/console-app/source/pages/gameserver/gameserver.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import { RouterLink, RouterOutlet } from '@angular/router'
 
 @Component({
   standalone: true,
   selector: 'console-gameserver',
   templateUrl: 'gameserver.component.html',
   styleUrl: 'gameserver.component.css',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
 })
 export class GameserverComponent {}
