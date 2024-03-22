9bddc2aa41e21fb3755fe658480ad5be236e1e28
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-22 10:07:39 +0100

    Add gameserver board components

---
+++ packages/console-app/source/pages/gameserver/components/uptime-board/gameserver-uptime-board.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  standalone: true,
+  selector: 'console-gameserver-uptime-board',
+  templateUrl: 'gameserver-uptime-board.component.html',
+  styleUrl: 'gameserver-uptime-board.component.css',
+  imports: [],
+})
+export class GameserverUptimeBoardComponent {}
---
+++ packages/console-app/source/pages/gameserver/components/vitals-board/gameserver-vitals-board.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  standalone: true,
+  selector: 'console-gameserver-vitals-board',
+  templateUrl: 'gameserver-vitals-board.component.html',
+  styleUrl: 'gameserver-vitals-board.component.css',
+  imports: [],
+})
+export class GameserverVitalsBoardComponent {}
