91d4d6b1596741cea295ff326f2f19302ecd70f0
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-22 10:16:07 +0100

    Fix invalid import identifier

--- packages/console-app/source/app.ts
+++ packages/console-app/source/app.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser'
 
 import { config } from './app.config'
-import { AppComponent } from './app.component'
+import { App } from './app.component'
 
-bootstrapApplication(AppComponent, config).catch((err: any) => console.error(err))
+bootstrapApplication(App, config).catch((err: any) => console.error(err))
