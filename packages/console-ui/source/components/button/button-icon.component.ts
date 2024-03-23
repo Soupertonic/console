bba2f6c2b110518aac9b4fdb6a55d453a519e53f
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-23 08:24:36 +0100

    Fully flesh out button icon component

--- packages/console-ui/source/components/button/button-icon.component.ts
+++ packages/console-ui/source/components/button/button-icon.component.ts
@@ -1,12 +1,15 @@
 import { Component, input } from "@angular/core"
+import { Icon } from "../icon"
 
 @Component({
   standalone: true,
   selector: 'console-button-icon',
   templateUrl: 'button-icon.component.html',
   styleUrl: 'button-icon.component.scss',
-  imports: [],
+  imports: [Icon],
 })
 export class ButtonIcon {
+  public identifier = input.required<string>()
+  public size = input.required<'small' | 'regular' | 'big'>()
   public slot = input.required<'start' | 'end'>()
 }
