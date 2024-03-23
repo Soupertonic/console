8b15c5b114d0b27fa6032d7e7cd585caa46a49ee
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-23 02:57:52 +0100

    Start filling game server menu with actual content

--- packages/console-app/source/pages/gameserver/components/menu-board/gameserver-menu-board.component.ts
+++ packages/console-app/source/pages/gameserver/components/menu-board/gameserver-menu-board.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
 import { RouterLink } from '@angular/router'
+import { Button, ButtonIcon, ButtonLabel } from '@console/ui/components/button'
 
 @Component({
   standalone: true,
   selector: 'console-gameserver-menu-board',
   templateUrl: 'gameserver-menu-board.component.html',
   styleUrl: 'gameserver-menu-board.component.scss',
-  imports: [RouterLink],
+  imports: [RouterLink, Button, ButtonIcon, ButtonLabel],
 })
 export class GameserverMenuBoard {}
