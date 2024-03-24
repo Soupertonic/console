5821538808b380308e2a5f1e59b9442190c301bb
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-24 21:20:59 +0100

    Add avatar mark capability

--- packages/console-app/source/components/app-navigation-user/app-navigation-user.component.ts
+++ packages/console-app/source/components/app-navigation-user/app-navigation-user.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 
 import { Avatar } from '@console/ui/components/avatar';
+import { AvatarMark } from '@console/ui/components/avatar-mark';
 import { Button } from '@console/ui/components/button';
 import { ButtonIcon } from '@console/ui/components/button-icon';
 
@@ -9,6 +10,6 @@ import { ButtonIcon } from '@console/ui/components/button-icon';
   selector: 'console-app-navigation-user',
   templateUrl: 'app-navigation-user.component.html',
   styleUrl: 'app-navigation-user.component.scss',
-  imports: [Avatar, Button, ButtonIcon],
+  imports: [Avatar, AvatarMark, Button, ButtonIcon],
 })
 export class AppNavigationAvatar {}
