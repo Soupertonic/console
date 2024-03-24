42f64a0b88d661e83b763ea3d5653418b7a74915
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-24 19:40:34 +0100

    Add main navigation menu

---
+++ packages/console-app/source/components/app-navigation-menu/app-navigation-menu.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Button } from '@console/ui/components/button';
+import { ButtonIcon } from '@console/ui/components/button-icon';
+
+@Component({
+  standalone: true,
+  selector: 'console-app-navigation-menu',
+  templateUrl: 'app-navigation-menu.component.html',
+  styleUrl: 'app-navigation-menu.component.scss',
+  imports: [Button, ButtonIcon],
+})
+export class AppNavigationMenu {
+  private _router = inject(Router)
+  private _activatedRoute = inject(ActivatedRoute)
+
+  protected navigateRoute(route: string): void {
+    this._router.navigate([route], { relativeTo: this._activatedRoute })
+  }
+
+  protected isActiveRoute(route: string): boolean {
+    return this._router.isActive(
+      this._router.createUrlTree([route], { relativeTo: this._activatedRoute }),
+      { paths: 'subset', queryParams: 'ignored', fragment: 'ignored', matrixParams: 'ignored' }
+    )
+  }
+}
--- packages/console-app/source/components/app-navigation/app-navigation.component.ts
+++ packages/console-app/source/components/app-navigation/app-navigation.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core'
 
 import { AppNavigationLogo } from '../app-navigation-logo';
+import { AppNavigationMenu } from '../app-navigation-menu';
 
 @Component({
   standalone: true,
   selector: 'console-app-navigation',
   templateUrl: 'app-navigation.component.html',
   styleUrl: 'app-navigation.component.scss',
-  imports: [AppNavigationLogo],
+  imports: [AppNavigationLogo, AppNavigationMenu],
 })
 export class AppNavigation {}
