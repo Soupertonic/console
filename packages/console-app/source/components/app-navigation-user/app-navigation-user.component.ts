b53e50d16fe4c563936de39381ae30ff83e9386c
Felix Scholz <felixscholz@xenoncloud.net>
2024-03-24 19:49:33 +0100

    Add main navigation user section

---
+++ packages/console-app/source/components/app-navigation-user/app-navigation-user.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+
+import { Avatar } from '@console/ui/components/avatar';
+import { Button } from '@console/ui/components/button';
+import { ButtonIcon } from '@console/ui/components/button-icon';
+
+@Component({
+  standalone: true,
+  selector: 'console-app-navigation-user',
+  templateUrl: 'app-navigation-user.component.html',
+  styleUrl: 'app-navigation-user.component.scss',
+  imports: [Avatar, Button, ButtonIcon],
+})
+export class AppNavigationAvatar {}
--- packages/console-app/source/components/app-navigation/app-navigation.component.ts
+++ packages/console-app/source/components/app-navigation/app-navigation.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core'
 
 import { AppNavigationLogo } from '../app-navigation-logo';
 import { AppNavigationMenu } from '../app-navigation-menu';
+import { AppNavigationAvatar } from '../app-navigation-user';
 
 @Component({
   standalone: true,
   selector: 'console-app-navigation',
   templateUrl: 'app-navigation.component.html',
   styleUrl: 'app-navigation.component.scss',
-  imports: [AppNavigationLogo, AppNavigationMenu],
+  imports: [AppNavigationLogo, AppNavigationMenu, AppNavigationAvatar],
 })
 export class AppNavigation {}
